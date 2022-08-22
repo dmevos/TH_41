ff5a90f0f5652b7596d55aaa90a15a76c5cb9a41
Dmitry Osipov <deosipov@mail.ru>
2022-08-22 19:38:52 +0300

    Задача 4.1

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,19 @@ import java.util.concurrent.BlockingQueue;
 public class Main {
     final static int COUNT_HELPER = 2;
     final static int CALLS_PER_DAY = 30;
-
+    final static int QUEUE_SIZE = 5;
+    final static int FREQ_OF_CALLS = 500;
+    final static int DURATION_OF_CONVERSATION = 3000;
 
     public static void main(String[] args) {
-        BlockingQueue<String> conversations = new ArrayBlockingQueue<>(5);
+        BlockingQueue<String> conversations = new ArrayBlockingQueue<>(QUEUE_SIZE);
 
         Thread ATS = new Thread(() -> {
             for (int i = 1; i <= CALLS_PER_DAY; i++) {
                 try {
                     conversations.put(Integer.toString(i));
                     System.out.println("Звонок " + i);
-                    Thread.sleep(500);
+                    Thread.sleep(FREQ_OF_CALLS);
                 } catch (InterruptedException e) {
                     return;
                 }
@@ -30,7 +32,7 @@ public class Main {
                 for (int j = 0; j < CALLS_PER_DAY / COUNT_HELPER; j++) {
                     try {
                         System.out.println("Обрабатываем звонок " + conversations.take());
-                        Thread.sleep(3000);
+                        Thread.sleep(DURATION_OF_CONVERSATION);
                     } catch (InterruptedException e) {
                         return;
                     }
